7e509b5a2cc3e5b3984e6154a88366bf09d04c02
VanderleiJunior <vanderleioleveira76@gmail.com>
2022-11-29 10:45:19 -0300

    get Temas e Comentarios

--- src/comments.routes.js
+++ src/comments.routes.js
@@ -22,15 +22,28 @@ commentsRoutes.post("/comments", async(request, response) => {
 
 //Ler os comentarios (read);
 commentsRoutes.get("/comments/:idTema", async(request, response) => {
-    const {idTema} = request.body;
+    const {idTema} = request.params;
 
-    const comments = await prisma.Comentario.findMany({where: {idTema}});
+    const comments = await prisma.Comentario.findMany({where: {idTema: Number(idTema)}});
 
     if(!comments) {
         return response.status(404).json("nao tem comentarios");
     }
 
     return response.status(200).json(comments);
+})
+
+//Ler os Temas
+commentsRoutes.get("/tema/:id", async(request, response) => {
+    const {id} = request.params;
+
+    const tema = await prisma.Tema.findUnique({ where: {id: Number(id)}});
+
+    if(!tema) {
+        return response.status(404).json("Tema inesistente");
+    }
+
+    return response.status(200).json(tema);
 })
 
 module.exports = commentsRoutes;
